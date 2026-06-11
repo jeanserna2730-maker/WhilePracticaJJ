9aa2fa31534016cac9dff57f309f042d4e213705
jeanserna2730-maker <jean.serna2730@utc.edu.ec>
2026-06-11 16:57:54 -0500

    Codigo arreglado

--- src/whilepracticajj/WhilePracticaJJ.java
+++ src/whilepracticajj/WhilePracticaJJ.java
@@ -4,77 +4,80 @@
  */
 package whilepracticajj;
 
-/**
- *
- * @author el_ingeniero
- */
-
 import java.util.Scanner;
 
 public class WhilePracticaJJ {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Practica con github y ciclo hile");
-        int opcion;
-        do {
-            System.out.println("1.suma");
-            System.out.println("2.resta");
-            System.out.println("ingrese una opcion:");
-            
-            opcion = sc.nextInt();
-            
-            switch (opcion){
-                case 1:
-                    System.out.println("funcion suma");
-                    break;
-                case 2:
-                    System.out.println("funcion resta");
-                    break;
-                default:
-                    System.out.println("opcion incorrecta");
-                    
-            }       
-            
-        } while (opcion <=3);
-    }
-    public static void cajero_automatico(){
-        Scanner scc = new Scanner(System.in);
-    //Algoritmo para simular cajero automatico
+public static void main(String[] args){
+    cajero_automatico();
+}
+public static void cajero_automatico() {
+        Scanner scc = new Scanner(System.in);  
+        
+        int clave = 1234;
+        int intentos = 3;
+        int opccion;
+        int intentos_opccion = 3;
+        
         System.out.println("""
                            -----------------------------------------------------
                                             CAJERO AUTOMATICO
+                            Ingrese su tarjeta
+                            Tarjeta detectada
+                            Ingresa tu clave:
                            """);
-        int clave = 1234;
-        int intentos = 3;
-        while (intentos > 0 && intentos<=3){
-        System.out.println("Ingrese su tarjeta:");
-        System.out.println("Tarjeta detectada");
-        System.out.println("Ingresa tu clave:");
-        int clave_ingresada = scc.nextInt();
-        if (clave_ingresada ==clave){
-        //Si la clave coincide
-            System.out.println("""
-                               -----------------------------------------------------
-                                               BIENVENIDO AL CAJERO
-                               """);
-            System.out.println("Selecciona una opcion:");
-            System.out.println("1. consultar saldo");
-            System.out.println("2. retirar dinero");
-            System.out.println("3. depositar dinero");
-            System.out.println("4. salir");
-        }else{
-            System.out.println("Si la clave es incorrecta:");
-            intentos = -1;
-            System.out.println("Te quedan" + intentos);
+        
+        while (intentos > 0 && intentos <=3) {
+            int clave_ingresada = scc.nextInt();
             
+            if (clave_ingresada == clave) {
+                // Si la clave coincide
+                do {
+                    System.out.println("""
+                                       -----------------------------------------------------
+                                                      BIENVENIDO AL CAJERO
+                                       -----------------------------------------------------
+                                       1. Consultar saldo
+                                       2. Retirar dinero
+                                       3. Depositar dinero
+                                       4. Salir
+                                       Ingrese la opcion deseada:
+                                       """);
+                    opccion = scc.nextInt();
+                    switch (opccion) {
+                        case 1:
+                            System.out.println("Consultar saldo:");
+                            consultar_saldo();
+                            break;
+                        case 2:
+                            System.out.println("Retirar dinero:");
+                            break;
+                        case 3:
+                            System.out.println("Depositar dinero:");
+                            break;
+                        case 4:
+                            System.out.println("Gracias por elegirnos!!!");
+                            break;
+                        default:
+                            intentos_opccion --;
+                            System.out.println("Te quedan "+intentos_opccion+" intentos!");
+                            System.out.println("INGRESE UNA OPCIÓN VALIDA!!!");
+                    }
+                } while (opccion != 4 && intentos_opccion >0); 
+                break;
+                
+            } else {
+                // Si la clave es incorrecta
+                intentos = intentos -1;
+                System.out.println("Clave incorrecta. Te quedan " + intentos + " intentos");
             }
-        if (intentos == 0){
-            System.out.println("Haz superado los 3 intentos..., ¿Eres Mongolo?");
+        }   
+        if (intentos == 0) {
+            System.out.println("Haz superado los 3 intentos...");
         }
-        }
-    } 
+        
+    }
+     public static void consultar_saldo(){
+         double saldo = 1000;
+         System.out.println("Saldo disponible: "+saldo);
+ }
 }
