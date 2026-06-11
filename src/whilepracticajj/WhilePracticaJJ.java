c2fb32c5bab455d37e2c386e1820f0a4ef58b96a
jeanserna2730-maker <jean.serna2730@utc.edu.ec>
2026-06-11 15:04:36 -0500

    Jairo

--- src/whilepracticajj/WhilePracticaJJ.java
+++ src/whilepracticajj/WhilePracticaJJ.java
@@ -8,13 +8,46 @@ package whilepracticajj;
  *
  * @author el_ingeniero
  */
+import java.util.Scanner;
 public class WhilePracticaJJ {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("practica con github y ciclo hile");
+        int opcion;
+        do {
+            System.out.println("1.suma");
+            System.out.println("2.resta");
+            System.out.println("ingrese una opcion");
+            
+            opcion = sc.nextInt();
+            
+            switch (opcion){
+                case 1:
+                    System.out.println("funcion suma");
+                    break;
+                case 2:
+                    System.out.println("funcion resta");
+                    break;
+                default:
+                    System.out.println("opcion incorrecta");
+                    
+            }       
+            
+        } while (opcion <=3);
     }
-    
+    public static void cajero_automatico(){
+    //Algoritmo para simular cajero automatico
+        System.out.println("""
+                           -----------------------------------------------------
+                                            CAJERO AUTOMATICO
+                           """);
+        System.out.println("Ingrese su tarjeta:");
+        System.out.println("Tarjeta detectada");
+        System.out.println("Ingresa tu clave:");
+        int clave = sc.nextint();
+    }
 }
